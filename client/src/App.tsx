7df492e813525e9ce76522718a5961df8a1a2842
Aaditya Patwari <aadipatwari@gmail.com>
2023-09-16 20:40:11 +0800

    Frontend basic navbar.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import { Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import styles from './App.module.css';
+import { useState } from 'react';
 
 const App: React.FC = () => {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [username, setUsername] = useState<string | undefined>();
+
   return (
-    <div className="App">
-      <Navbar />
+    <div className={styles.App}>
+      <Navbar loggedIn={loggedIn} username={username} />
       <Routes></Routes>
     </div>
   );
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,38 @@
-const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+import { NavLink } from 'react-router-dom';
+import styles from './Navbar.module.css';
+
+interface NavbarProps {
+  loggedIn: boolean;
+  username?: string;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ loggedIn, username }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.logo}>URL Shortener</div>
+      <div className={styles.links}>
+        {loggedIn ? (
+          <>
+            <div className={styles.link}>
+              <NavLink to="/manage">Manage</NavLink>
+            </div>
+            <div className={styles.link}>
+              <NavLink to="/logout">Logout</NavLink>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.link}>
+              <NavLink to="/login">Login</NavLink>
+            </div>
+            <div className={styles.link}>
+              <NavLink to="/signup">Sign Up</NavLink>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
