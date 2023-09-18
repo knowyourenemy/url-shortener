49e4a0af8999c44ce694d4a5cd7ffa8cdde24dcd
Aaditya Patwari <aadipatwari@gmail.com>
2023-09-19 01:02:28 +0800

    backend add url db and route tests.

---
+++ server/__tests__/models/url.db.test.ts
@@ -0,0 +1,42 @@
+import { ObjectId } from 'mongodb';
+import { connectToDatabase, disconnect, getUrlCollection } from '../../src/db';
+import { checkOriginalUrlExists, getUrl, insertUrl } from '../../src/models/url.db';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+beforeEach(async () => {
+  // Clear URL Collection before each test.
+  await getUrlCollection().deleteMany();
+});
+
+const MOCK_ORIGINAL_URL = 'mock-original-url';
+const MOCK_SHORT_URL = 'mock-short-url';
+const MOCK_USER_ID = new ObjectId();
+
+describe('Test insertUrl()', () => {
+  it('should successfully insert a URL', async () => {
+    await insertUrl({
+      shortenedUrl: MOCK_SHORT_URL,
+      originalUrl: MOCK_ORIGINAL_URL,
+      userId: MOCK_USER_ID,
+    });
+    const url = await getUrl(MOCK_SHORT_URL);
+    expect(url.originalUrl).toBe(MOCK_ORIGINAL_URL);
+  });
+});
+
+describe('Test getOriginalUrlExists', () => {
+  it('should return false for URL that does not exist', async () => {
+    const urlExists = await checkOriginalUrlExists(MOCK_ORIGINAL_URL, MOCK_USER_ID);
+    expect(urlExists).toBe(false);
+  });
+
+  it('should return true for URL that exist', async () => {
+    await insertUrl({
+      shortenedUrl: MOCK_SHORT_URL,
+      originalUrl: MOCK_ORIGINAL_URL,
+      userId: MOCK_USER_ID,
+    });
+    const urlExists = await checkOriginalUrlExists(MOCK_ORIGINAL_URL, MOCK_USER_ID);
+    expect(urlExists).toBe(true);
+  });
+});
---
+++ server/__tests__/routes/url.test.ts
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import { makeApp } from '../../src/app';
+import { insertUrl } from '../../src/models/url.db';
+import { ObjectId } from 'mongodb';
+import { getUrlCollection } from '../../src/db';
+
+const app = makeApp();
+
+const MOCK_ORIGINAL_URL = 'mock-original-url';
+const MOCK_SHORT_URL = 'mock-short-url';
+const MOCK_USER_ID = new ObjectId();
+
+beforeEach(async () => {
+  await getUrlCollection().deleteMany();
+});
+
+describe('GET /api/url/:shortenedUrl', () => {
+  it('should redirect to original URL', async () => {
+    await insertUrl({
+      shortenedUrl: MOCK_SHORT_URL,
+      originalUrl: MOCK_ORIGINAL_URL,
+      userId: MOCK_USER_ID,
+    });
+    const res = await request(app).get(`/api/url/${MOCK_SHORT_URL}`);
+    expect(res.statusCode).toBe(302);
+  });
+
+  it('should throw a 404 error for URL that does not exist', async () => {
+    const res = await request(app).get(`/api/url/${MOCK_SHORT_URL}`);
+    expect(res.statusCode).toBe(404);
+  });
+});
