c0b958ca9086588281af6f9f6cd5caf797324d20
Aaditya Patwari <aadipatwari@gmail.com>
2023-09-19 01:01:25 +0800

    Backend extract app to separate file.

---
+++ server/src/app.ts
@@ -0,0 +1,34 @@
+import express, { Express, NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { connectToDatabase } from './db';
+import { AppError, MissingEnvError } from './util/appError';
+import userRouter from './routes/user';
+import urlRouter from './routes/url';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+export const makeApp = () => {
+  const app: Express = express();
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(
+    cors({
+      origin: ['http://localhost:3000', 'https://shrtn.net'],
+      credentials: true,
+    }),
+  );
+  app.get('/', (req: Request, res: Response) => {
+    res.send('URL Shortener');
+  });
+  app.use('/api/user', userRouter);
+  app.use('/api/url', urlRouter);
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+    if (err instanceof AppError) {
+      return res.status(err.statusCode).send(err.message);
+    } else {
+      return res.sendStatus(500);
+    }
+  });
+  return app;
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import userRouter from './routes/user';
 import urlRouter from './routes/url';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { makeApp } from './app';
 
 dotenv.config();
 
@@ -16,30 +17,10 @@ const serveApp = async () => {
   if (!process.env.DB_CONN_STRING) {
     throw new MissingEnvError();
   }
-  const app: Express = express();
+  const app: Express = makeApp();
   const port = process.env.PORT;
   await connectToDatabase(process.env.DB_CONN_STRING);
-  app.use(express.json());
-  app.use(cookieParser());
-  app.use(
-    cors({
-      origin: ['http://localhost:3000', 'https://shrtn.net'],
-      credentials: true,
-    }),
-  );
-  app.get('/', (req: Request, res: Response) => {
-    res.send('URL Shortener');
-  });
-  app.use('/api/user', userRouter);
-  app.use('/api/url', urlRouter);
-  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err);
-    if (err instanceof AppError) {
-      return res.status(err.statusCode).send(err.message);
-    } else {
-      return res.sendStatus(500);
-    }
-  });
+
   app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
   });
